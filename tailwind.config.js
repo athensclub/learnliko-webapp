caeeb7256888a1ab101e174443b48ac2ffb220b1
athensclub <43963760+athensclub@users.noreply.github.com>
2023-04-14 18:09:50 +0700

    edit: box shadow config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    fontFamily: { "line-seed": "LINE Seed Sans TH" },
-    extend: {
-      transitionProperty: {
-        size: "width, height",
-        font: "font"
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: { 'line-seed': 'LINE Seed Sans TH' },
+		extend: {
+			transitionProperty: {
+				size: 'width, height',
+				font: 'font'
+			},
+			boxShadow: {
+				'all': '0px 1px 8px 0px #00000040'
+			}
+		}
+	},
+	plugins: []
 };

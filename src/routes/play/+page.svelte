4ff177a09b57334ec62ceb6327cc90eaa2377681
athensclub <43963760+athensclub@users.noreply.github.com>
2023-04-14 18:09:41 +0700

    edit: no longer use outside div template

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,29 +1,20 @@
 <script lang="ts">
-  import CardSlider from "$lib/components/CardSlider.svelte";
-  import Select from "svelte-select";
+	import CardSlider from '$lib/components/CardSlider.svelte';
+	import Select from 'svelte-select';
 
-  let options = [
-    "Grade 1",
-    "Grade 2",
-    "Grade 3",
-    "Grade 4",
-    "Grade 5",
-    "Grade 6",
-  ];
+	let options = ['Grade 1', 'Grade 2', 'Grade 3', 'Grade 4', 'Grade 5', 'Grade 6'];
 </script>
 
-<div class="w-[100vw] h-[1px] min-h-[100vh]">
-  <header
-    class="flex items-center justify-center border-b border-black h-[48px] font-line-seed text-xl font-bold"
-  >
-    Learnliko.ai
-  </header>
+<header
+	class="flex items-center justify-center border-b border-black h-[48px] font-line-seed text-xl font-bold"
+>
+	Learnliko.ai
+</header>
 
-  <div class="w-full flex flex-row justify-end">
-    <div class="w-[15%] mr-5 mt-3">
-      <Select items={options} placeholder="Select Level" />
-    </div>
-  </div>
-
-  <CardSlider />
+<div class="w-full flex flex-row justify-end">
+	<div class="w-[15%] mr-5 mt-3">
+		<Select items={options} placeholder="Select Level" />
+	</div>
 </div>
+
+<CardSlider />

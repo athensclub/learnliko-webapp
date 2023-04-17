caf74f8ac56b2816e483b35950c3219efb82ad5c
athensclub <athens_club@hotmail.com>
2023-04-18 01:40:27 +0700

    add: confirm modal type

---
+++ src/lib/components/modals/ConfirmModal.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { closeModal } from '$lib/global/modal';
+
+	export let title: string;
+	export let description: string;
+
+	export let onConfirm: () => void;
+
+	const handleConfirmClicked = () => {
+		onConfirm();
+		closeModal();
+	};
+</script>
+
+<div class="w-full h-full bg-white font-line-seed flex flex-col">
+
+	<div class="font-bold text-xl text-center">{title}</div>
+	<div class="text-lg mt-1 text-center">{description}</div>
+	<div class="w-full flex flex-row justify-end mr-10">
+		<button on:click={closeModal} class="px-1 py-2 mr-3">Cancel</button>
+		<button on:click={handleConfirmClicked} class="px-1 py-2 mr-3">OK</button>
+	</div>
+</div>

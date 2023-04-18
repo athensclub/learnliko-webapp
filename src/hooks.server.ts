a9d2ce857762e130cf6c0e44c0e309f1fea730c0
kithe-raker <kithe.raker@gmail.com>
2023-04-18 17:14:03 +0700

    edit: enable cors on `api` route

---
+++ src/hooks.server.ts
@@ -0,0 +1,22 @@
+import { isOnProduction } from '$lib/utils/environment';
+import type { Handle } from '@sveltejs/kit';
+
+const _whitelist = isOnProduction()
+	? ['https://learnliko.pages.dev']
+	: ['http://localhost:5173', 'https://staging.learnliko.pages.dev'];
+
+export const handle: Handle = async ({ resolve, event }) => {
+	const response = await resolve(event);
+
+	// Apply CORS header for API routes
+	if (event.url.pathname.startsWith('/api')) {
+		const isOnWhitelist = _whitelist.includes(event.url.origin);
+
+		// If isn't on the whitelist return Unauthorized response
+		if (!isOnWhitelist) {
+			return new Response('Unauthorized', { status: 401 });
+		}
+	}
+
+	return response;
+};

1263f98902a2db7b32dc375b71f111904f2b96d2
athensclub <athens_club@hotmail.com>
2023-04-19 00:35:12 +0700

    edit: transcribe api function documentation

--- src/routes/api/_lib/transcription.ts
+++ src/routes/api/_lib/transcription.ts
@@ -1,3 +1,8 @@
+/**
+ * Transcribe the given audio source to text.
+ * @param audio the audio source
+ * @returns the text transcribed from the audio.
+ */
 export const transcribe = async function (audio: Blob): Promise<string> {
 	const response = await fetch('/api/v1/transcribe', {
 		method: 'POST',

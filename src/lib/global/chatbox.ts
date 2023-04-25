8a697c20024e403df6a3c2034f4514d61a838a7d
athensclub <athens_club@hotmail.com>
2023-04-25 23:48:23 +0700

    edit: recap history item type

--- src/lib/global/chatbox.ts
+++ src/lib/global/chatbox.ts
@@ -7,13 +7,28 @@ export type ChatboxView = "CONVERSATION" | "RECAP";
  */
 export const showChatbox = writable(false);
 
-export type RecapHistory = {
-    role: 'user' | 'assistant';
-    audioURL: string;
-    transcription: string;
+/**
+ * pair of assistant message, then user message.
+ */
+export type RecapHistoryItem = {
+    assistant: {
+        role: 'assistant';
+        audioURL: string;
+        transcription: string;
+    };
+    user: {
+        role: 'user';
+        audioURL: string;
+        transcription: string;
+    } | null;
     suggestion: string;
     score: number;
-}[];
+};
+
+/**
+ * A history of pair of assistant message, then user message.
+ */
+export type RecapHistory = RecapHistoryItem[];
 /**
  * A history to be used to show in recap.
  * Fill this store's data before showing recap view.

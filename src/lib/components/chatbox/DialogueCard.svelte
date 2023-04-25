5993156e6d16311c1da8768b3049fd1217dff657
athensclub <athens_club@hotmail.com>
2023-04-25 23:48:03 +0700

    add: dialogue card

---
+++ src/lib/components/chatbox/DialogueCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { RecapHistoryItem } from "$lib/global/chatbox";
+
+	export let dialogueNumber: number;
+    export let dialogue: RecapHistoryItem;
+</script>
+
+<div class="w-[95%] mx-auto mt-3 rounded-xl flex flex-col bg-[#F8F8F8]">
+	<div class="flex flex-row justify-between text-xl font-bold items-center px-3">
+		Dialogue {dialogueNumber}
+
+		<div class="rounded-lg bg-black text-base text-center text-white px-2">{dialogue.score} Point</div>
+	</div>
+</div>

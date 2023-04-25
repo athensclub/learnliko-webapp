4478df6c9fb487e62ee0c31de44216a89f812e4b
athensclub <athens_club@hotmail.com>
2023-04-25 23:03:57 +0700

    add: recap history store object to be used with recap view

--- src/lib/global/chatbox.ts
+++ src/lib/global/chatbox.ts
@@ -1,6 +1,21 @@
 import { writable } from "svelte/store";
 
+export type ChatboxView = "CONVERSATION" | "RECAP";
+
 /**
  * Whether to show the chatbox or not.
  */
 export const showChatbox = writable(false);
+
+export type RecapHistory = {
+    role: 'user' | 'assistant';
+    audioURL: string;
+    transcription: string;
+    suggestion: string;
+    score: number;
+}[];
+/**
+ * A history to be used to show in recap.
+ * Fill this store's data before showing recap view.
+ */
+export const recapHistory = writable<RecapHistory>([]);

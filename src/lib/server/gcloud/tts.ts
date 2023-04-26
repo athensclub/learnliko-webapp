1af6379d7415951dee594dbcee4b429a4dd7fce2
kithe-raker <kithe.raker@gmail.com>
2023-04-27 03:17:07 +0700

    edit: create `TextToSpeechClient` instance by using `SECRET_GOOGLE_SERVICE_ACCOUNT`

--- src/lib/server/gcloud/tts.ts
+++ src/lib/server/gcloud/tts.ts
@@ -1,21 +1,35 @@
+import { SECRET_GOOGLE_SERVICE_ACCOUNT } from '$env/static/private';
 import textToSpeech from '@google-cloud/text-to-speech';
 import type { google } from '@google-cloud/text-to-speech/build/protos/protos';
 
-const client = new textToSpeech.TextToSpeechClient();
+const client = new textToSpeech.TextToSpeechClient({
+	credentials: JSON.parse(SECRET_GOOGLE_SERVICE_ACCOUNT)
+});
 
-export const synthesize = async (text: string, languageCode: string, voiceName: string, ssmlGender: "SSML_VOICE_GENDER_UNSPECIFIED" | "MALE" | "FEMALE" | "NEUTRAL" | google.cloud.texttospeech.v1.SsmlVoiceGender) => {
-    try {
-        const [response] = await client.synthesizeSpeech({
-            input: { text: text },
-            voice: { languageCode, ssmlGender, name: voiceName },
-            audioConfig: { audioEncoding: 'OGG_OPUS' },
-        }); if (response.audioContent) {
-            return new Blob([response.audioContent]);
-        } else {
-            throw new Error("Synthesize does not return any audio content (input text: " + text + ")");
-        }
-    } catch (e) {
-        console.log(e)
-        throw e;
-    }
-}
+export const synthesize = async (
+	text: string,
+	languageCode: string,
+	voiceName: string,
+	ssmlGender:
+		| 'SSML_VOICE_GENDER_UNSPECIFIED'
+		| 'MALE'
+		| 'FEMALE'
+		| 'NEUTRAL'
+		| google.cloud.texttospeech.v1.SsmlVoiceGender
+) => {
+	try {
+		const [response] = await client.synthesizeSpeech({
+			input: { text: text },
+			voice: { languageCode, ssmlGender, name: voiceName },
+			audioConfig: { audioEncoding: 'OGG_OPUS' }
+		});
+		if (response.audioContent) {
+			return new Blob([response.audioContent]);
+		} else {
+			throw new Error('Synthesize does not return any audio content (input text: ' + text + ')');
+		}
+	} catch (e) {
+		console.log(e);
+		throw e;
+	}
+};

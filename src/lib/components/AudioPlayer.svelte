7edf35536bea3c74c6bf61343efec7b4ccd6dbe7
Kithe <57142176+SB051@users.noreply.github.com>
2023-05-04 10:33:28 +0700

    styling audio player

--- src/lib/components/AudioPlayer.svelte
+++ src/lib/components/AudioPlayer.svelte
@@ -3,7 +3,17 @@
 	export let type = 'audio/ogg; codecs=opus';
 </script>
 
-<audio controls>
+<audio
+	controls
+	style="border:10px solid #B8DDFF; 
+	border-radius: 50px;"
+>
 	<source {src} {type} />
 	Your browser does not support the audio element.
 </audio>
+
+<style>
+	audio::-webkit-media-controls-panel {
+  background-color: #56AEFF;
+}
+</style>

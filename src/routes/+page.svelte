9cc0ebf5072d65e85b93c8a6517ed98b90723ae1
Kithe <57142176+SB051@users.noreply.github.com>
2023-05-05 13:35:47 +0700

    add homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
+	
 
-	if (browser) {
-		goto('/play');
-	}
+	// if (browser) {
+	// 	goto('/play');
+	// }
 </script>
 
-<h1 class="text-purple-500 font-bold">Welcome to Learnliko</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<template>
+	<div>
+	  <html>
+		<!-- path to your HTML file goes here -->
+		<iframe src="../index.html" frameborder="0" width="200" height="300"></iframe>
+	  </html>
+	</div>
+  </template>

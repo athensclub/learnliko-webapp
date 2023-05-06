8adc8112aef161c89aebae10d6569f7594e699a0
kithe-raker <kithe.raker@gmail.com>
2023-05-06 20:06:53 +0700

    edit: add `app.learnliko.com` to whitelist

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,16 +2,16 @@ import { isOnProduction } from '$lib/utils/environment';
 import type { Handle } from '@sveltejs/kit';
 
 const _whitelist = isOnProduction()
-	? ['https://learnliko.pages.dev', 'https://learnliko.netlify.app']
+	? ['https://learnliko.pages.dev', 'https://learnliko.netlify.app', 'https://app.learnliko.com']
 	: [
-		'http://localhost:5173',
-		'http://localhost:4173',
-		'http://[::]:5173',
-		'http://192.168.2.25:5173',
-		'http://127.0.0.1:5173',
-		'https://staging.learnliko.pages.dev',
-		'https://staging--learnliko.netlify.app'
-	];
+			'http://localhost:5173',
+			'http://localhost:4173',
+			'http://[::]:5173',
+			'http://192.168.2.25:5173',
+			'http://127.0.0.1:5173',
+			'https://staging.learnliko.pages.dev',
+			'https://staging--learnliko.netlify.app'
+	  ];
 
 export const handle: Handle = async ({ resolve, event }) => {
 	const response = await resolve(event);

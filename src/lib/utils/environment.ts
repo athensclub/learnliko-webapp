fee1b0f758875196f66eede110248a3dc19e9034
kithe-raker <kithe.raker@gmail.com>
2023-05-07 13:30:01 +0700

    test: log node env

--- src/lib/utils/environment.ts
+++ src/lib/utils/environment.ts
@@ -1,5 +1,6 @@
 import { PUBLIC_APP_ENV } from '$env/static/public';
 
 export const isOnProduction = function () {
+	console.log('Node env: ', process.env.NODE_ENV);
 	return process.env.NODE_ENV === 'production' && PUBLIC_APP_ENV === 'production';
 };

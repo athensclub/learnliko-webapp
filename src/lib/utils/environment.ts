33e5cb7bb1c724abfc674fd3aa01afeb96b39721
kithe-raker <kithe.raker@gmail.com>
2023-05-07 17:31:22 +0700

    edit: check `isOnProduction` without check node env

--- src/lib/utils/environment.ts
+++ src/lib/utils/environment.ts
@@ -1,6 +1,5 @@
 import { PUBLIC_APP_ENV } from '$env/static/public';
 
 export const isOnProduction = function () {
-	console.log('Node env: ', process.env.NODE_ENV);
-	return process.env.NODE_ENV === 'production' && PUBLIC_APP_ENV === 'production';
+	return PUBLIC_APP_ENV === 'production';
 };

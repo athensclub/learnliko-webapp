82f972cb00902d482180a5405dfa204130c3ac9d
kithe-raker <kithe.raker@gmail.com>
2023-05-09 23:22:39 +0700

    fix: retry `transcribe` audio if error

--- src/routes/api/_lib/transcription.ts
+++ src/routes/api/_lib/transcription.ts
@@ -4,9 +4,25 @@
  * @returns the text transcribed from the audio.
  */
 export const transcribe = async function (audio: Blob): Promise<string> {
-	const response = await fetch('/api/v1/transcribe', {
-		method: 'POST',
-		body: audio
-	});
-	return response.text();
+	let returnedData = '';
+
+	let attempt = 0;
+	while (true) {
+		try {
+			const response = await fetch('/api/v1/transcribe', {
+				method: 'POST',
+				body: audio
+			});
+
+			returnedData = await response.text();
+			break;
+		} catch (error) {
+			// max attempt at 5
+			if (attempt++ >= 5) break;
+
+			console.error('error: transcribe audio, retring...');
+		}
+	}
+
+	return returnedData;
 };

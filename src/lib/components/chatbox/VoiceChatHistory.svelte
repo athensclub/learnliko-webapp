78647226bb31f08360d527192c542a344458c525
Kithe <57142176+SB051@users.noreply.github.com>
2023-05-10 17:56:09 +0700

    fix ui

--- src/lib/components/chatbox/VoiceChatHistory.svelte
+++ src/lib/components/chatbox/VoiceChatHistory.svelte
@@ -15,7 +15,7 @@
 
 {#each history as chat, index (index)}
 
-	<div class={`flex flex-col w-full ${chat.role === 'user' ? 'items-end' : 'items-start'}`}>
+	<div class={`flex flex-col px-4 w-full ${chat.role === 'user' ? 'items-end' : 'items-start'}`}>
 		<div
 			class={`flex pt-6 flex-row items-center lg:w-[85%] w-[100%] ${
 				chat.role === 'user' ? 'justify-end' : 'justify-start'
@@ -23,24 +23,25 @@
 		>
 			{#if chat.role === 'assistant'}
 				<div
-					class={`w-[32px] h-[32px]  bg-center bg-cover rounded-full border border-white`}
+					class={`w-[42px] h-[42px]  bg-center bg-cover rounded-full border border-white`}
 					style="background-image: url('{assistantProfileImage}');"
 				/>
 			{/if}
-			<div class="w-[-75%] mx-2 ">
-				<AudioPlayer src={chat.audioURL} />
-			</div>
+		
 			{#if chat.role === 'user'}
 				<div
-					class={`w-[32px] h-[32px] bg-center bg-cover  rounded-full border border-white`}
+					class={`w-[42px] h-[42px] bg-center bg-cover  rounded-full border border-white`}
 					style="background-image: url('{userProfileImage}');"
 				/>
 			{/if}
 		</div>
+		<div class="w-[-75%] pt-3 ">
+			<AudioPlayer src={chat.audioURL} />
+		</div>
 		{#if chat.role === 'user' || (chat.role === 'assistant' && showAssistantTranscription)}
 			<div
 				class={`mt-3 flex flex-row w-[100%] ${
-					chat.role === 'assistant' ? 'pl-16 justify-start' : 'pr-16 justify-end'
+					chat.role === 'assistant' ? ' justify-start' : 'justify-end'
 				}`}
 			>
 				{#if chat.transcription === null}

6a9a42226ca7e2c9f8fd03ba9e9ac63be5301a4e
athensclub <athens_club@hotmail.com>
2023-05-26 03:10:51 +0700

    add: query conversation function using local indexeddb implementation

--- src/lib/localdb/conversationLocal.ts
+++ src/lib/localdb/conversationLocal.ts
@@ -1,3 +1,15 @@
+import { getConversations } from "$api/conversation";
+import { get } from "svelte/store";
+import { completedConversations } from "./profileLocal";
+
 /**
  * Conversation APIs implemented using IndexDB (For demo/testing only).
  */
+export const queryConversationsLocal = async () => {
+    if (!completedConversations)
+        throw new Error("do not query local data from ssr");
+
+    const conversations = await getConversations();
+    const completed = get(completedConversations);
+    return conversations.filter(conv => completed.every(com => com.conversation.conversationID !== conv.id));
+}

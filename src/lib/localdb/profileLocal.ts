50f1a938f9044c0f7d0c52f842f32b4b7c19eca5
athensclub <athens_club@hotmail.com>
2023-05-26 01:43:27 +0700

    edit: querying diary items also return each items learned

--- src/lib/localdb/profileLocal.ts
+++ src/lib/localdb/profileLocal.ts
@@ -1,17 +1,23 @@
 /**
  * Profile APIs implemented using IndexDB (For demo/testing only).
  */
-import { getConversations } from "$api/conversation";
+import { getConversations, getVocabsFromConversation } from "$api/conversation";
 import { browser } from "$app/environment";
-import type { FinishedConversation } from "$lib/types/conversationData";
-import type { LearningDiaryItem } from "$lib/types/learningDiary";
+import type { ConversationCarouselItem, FinishedConversation } from "$lib/types/conversationData";
+import type { LearnedItem, LearningDiaryItem } from "$lib/types/learningDiary";
 import { persist, createIndexedDBStorage } from "@macfja/svelte-persistent-store"
 import { writable, get } from "svelte/store"
 
-const completedConversations = browser ? persist(writable<FinishedConversation[]>([]), createIndexedDBStorage(), "completedConversations") : null;
+interface UserFinishedConversationItem {
+    conversation: FinishedConversation;
+    vocabs: string[];
+}
 
-export const completeConversationLocal = (conversation: FinishedConversation) => {
-    completedConversations?.set([...get(completedConversations), conversation]);
+const completedConversations = browser ? persist(writable<UserFinishedConversationItem[]>([]), createIndexedDBStorage(), "completedConversations") : null;
+
+export const completeConversationLocal = async (conversation: FinishedConversation) => {
+    completedConversations?.set([...get(completedConversations),
+    { conversation, vocabs: await getVocabsFromConversation(conversation.recap) }]);
 }
 
 export const queryLearningDiariesLocal = async (): Promise<LearningDiaryItem[]> => {
@@ -20,20 +26,32 @@ export const queryLearningDiariesLocal = async (): Promise<LearningDiaryItem[]> 
 
     const conversations = await getConversations();
     const completed = get(completedConversations);
-    const dateToConversations = new Map<string, FinishedConversation[]>;
-    for (const conv of completed) {
-        const date = `${conv.finishedTime.getDate()}.${conv.finishedTime.getMonth() + 1}.${conv.finishedTime.getFullYear()}`;
+    const dateToConversations = new Map<string, UserFinishedConversationItem[]>;
+    for (const item of completed) {
+        const date = `${item.conversation.finishedTime.getDate()}.${item.conversation.finishedTime.getMonth() + 1}.${item.conversation.finishedTime.getFullYear()}`;
         if (!dateToConversations.has(date))
             dateToConversations.set(date, []);
-        dateToConversations.get(date)?.push(conv);
+        dateToConversations.get(date)?.push(item);
     }
 
     const result: LearningDiaryItem[] = [];
     for (const key of dateToConversations.keys()) {
-        result.push({
-            date: key,
-            title: `You achieved following goal(s): ${dateToConversations.get(key)?.map(conv => conversations.find(c => c.id === conv.conversationID)?.details.learner.goal).join(", ")}`
-        });
+        const convs = dateToConversations.get(key);
+        // use if check to make linter happy :(z
+        if (convs) {
+            const items: LearnedItem[] = convs.map(conv =>
+            ({
+                recap: conv.conversation.recap,
+                conversation: conversations.find(c => c.id === conv.conversation.conversationID)!,
+                vocabs: conv.vocabs,
+                finishedTime: conv.conversation.finishedTime
+            }));
+            result.push({
+                date: key,
+                title: `You achieved following goal(s): ${items.map(item => item.conversation.details.learner.goal).join(", ")}`,
+                learnedItems: items
+            });
+        }
     }
     return result;
 };
--- src/lib/types/learningDiary.ts
+++ src/lib/types/learningDiary.ts
@@ -1,4 +1,15 @@
+import type { RecapHistory } from "$lib/global/chatbox";
+import type { ConversationCarouselItem } from "./conversationData";
+
 export interface LearningDiaryItem{
     date: string;
     title: string;
+    learnedItems: LearnedItem[]
+}
+
+export interface LearnedItem {
+    conversation: ConversationCarouselItem;
+    recap: RecapHistory;
+    vocabs: string[];
+    finishedTime: Date;
 }

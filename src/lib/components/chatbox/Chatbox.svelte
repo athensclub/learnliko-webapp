6a58e4d8c731d00bc4c9fad52504ee8a8e58231f
athensclub <athens_club@hotmail.com>
2023-06-04 03:19:12 +0700

    edit: render assistant view when it is current view

--- src/lib/components/chatbox/Chatbox.svelte
+++ src/lib/components/chatbox/Chatbox.svelte
@@ -3,20 +3,22 @@
 	import ConversationView from './ConversationView.svelte';
 	import { chatContext, currentChatboxView } from '$lib/global/chatbox';
 	import RecapView from './RecapView.svelte';
+	import AssistantView from './AssistantView.svelte';
 </script>
 
 <div
 	transition:fly={{ y: 800, duration: 800 }}
-
 	class="w-full h-full bg-white z-30 font-line-seed relative flex flex-col items-center shadow-2xl rounded-t-xl shadow-gray-700 border-[1px] border-black/10 border-b-0 px-2"
 >
-	{#if $chatContext}
-		{#if $currentChatboxView === 'CONVERSATION'}
-			<ConversationView  />
-		{:else if $currentChatboxView === 'RECAP'}
-			<RecapView  />
+	{#if $currentChatboxView === 'CONVERSATION'}
+		{#if $chatContext}
+			<ConversationView />
+		{:else}
+			<h3>No Data Available</h3>
 		{/if}
-	{:else}
-		<h3>No Data Available</h3>
+	{:else if $currentChatboxView === 'RECAP'}
+		<RecapView />
+	{:else if $currentChatboxView === 'ASSISTANT'}
+		<AssistantView />
 	{/if}
 </div>

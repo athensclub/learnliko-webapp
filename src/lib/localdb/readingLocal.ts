fff4b4e4f84c4ae350ebd2d0ab7c648ae68933ca
athensclub <athens_club@hotmail.com>
2023-06-03 17:32:17 +0700

    add: reading apis using local db implementation (indexeddb)

---
+++ src/lib/localdb/readingLocal.ts
@@ -0,0 +1,19 @@
+import { getReadingItems } from "$api/reading";
+import { browser } from "$app/environment";
+import type { FinishedReading } from "$lib/types/reading";
+import { createIndexedDBStorage, persist } from "@macfja/svelte-persistent-store";
+import { writable, get } from "svelte/store";
+
+export const completedReadings = browser ? persist(writable<FinishedReading[]>([]), createIndexedDBStorage(), "completedReadings") : null;
+
+export const completeReading = async (item: FinishedReading) => {
+    if (!completedReadings)
+        throw new Error("do not query local data from ssr");
+
+    completedReadings.set([...get(completedReadings), item]);
+};
+
+export const queryAnswers = async (readingID: string) => {
+    const all = await getReadingItems('All');
+    return all.find(v => v.id === readingID)?.quiz.map(q => q.answer);
+}

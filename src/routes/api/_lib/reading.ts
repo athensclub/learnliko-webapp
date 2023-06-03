d43f2855aaf74d2bc1479ae927a77eacbdc7ad2f
athensclub <athens_club@hotmail.com>
2023-06-03 17:32:48 +0700

    add: typing for reading sdk lib

--- src/routes/api/_lib/reading.ts
+++ src/routes/api/_lib/reading.ts
@@ -1,11 +1,12 @@
+import type { ReadingItem } from "$lib/types/reading";
 
-export const getReadingItems = async (topic: string) => {
+export const getReadingItems = async (topic: string): Promise<ReadingItem[]> => {
     const response = await fetch("/api/v1/reading?" + new URLSearchParams({ topic }));
     const val = await response.json();
     return val;
 };
 
-export const getReadingItemById = async (id: string) => {
+export const getReadingItemById = async (id: string): Promise<ReadingItem> => {
     const response = await fetch("/api/v1/reading?" + new URLSearchParams({ id }));
     const val = await response.json();
     return val;

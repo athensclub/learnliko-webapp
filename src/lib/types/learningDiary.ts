20a273613e575ac4675fed4bd5d79e817689224b
athensclub <athens_club@hotmail.com>
2023-06-03 20:57:55 +0700

    add: type properties to support learned reading item in diary

--- src/lib/types/learningDiary.ts
+++ src/lib/types/learningDiary.ts
@@ -1,13 +1,21 @@
 import type { RecapHistory } from "$lib/global/chatbox";
 import type { ConversationCarouselItem } from "./conversationData";
+import type { ReadingItem } from "./reading";
 
 export interface LearningDiaryItem{
     date: string;
     title: string;
-    learnedItems: LearnedItem[]
+    learnedConversations: LearnedConversationItem[];
+    learnedReadings: LearnedReadingItem[];
 }
 
-export interface LearnedItem {
+export interface LearnedReadingItem {
+    item: ReadingItem;
+    vocabs: string[];
+    finishedTime: Date;
+}
+
+export interface LearnedConversationItem {
     conversation: ConversationCarouselItem;
     recap: RecapHistory;
     vocabs: string[];

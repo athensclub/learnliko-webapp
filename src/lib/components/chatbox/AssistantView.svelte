bb64c399152879e444758d10ae9b15007b6f9ff4
athensclub <athens_club@hotmail.com>
2023-06-05 02:04:03 +0700

    feat: hint prompt and assistant languages

--- src/lib/components/chatbox/AssistantView.svelte
+++ src/lib/components/chatbox/AssistantView.svelte
@@ -19,6 +19,34 @@
 	let currentText = '';
 	let waitingForAIResponse = false;
 
+	let hintPrompts = ['learnliko คืออะไร', 'ฉันจะเริ่มเล่นบทสนทนาอย่างไร'];
+
+	let currentLanguage: 'TH' | 'EN' = 'TH';
+	const updateCurrentLanguage = () => {
+		if (currentLanguage === 'TH') {
+			hintPrompts = ['learnliko คืออะไร', 'ฉันจะเริ่มเล่นบทสนทนาอย่างไร'];
+			history = [
+				{
+					role: 'system',
+					content:
+						'You are a female assistant for website Learnliko. You must assist the user in using the website using given information about the website. All of your answers must be in Thai language. Do not answer in English language.'
+				},
+				{ role: 'assistant', content: 'สวัสดี มีอะไรให้ฉันช่วยบ้าง' }
+			];
+		} else if (currentLanguage === 'EN') {
+			hintPrompts = ['What is learnliko?', 'How to start conversation?'];
+			history = [
+				{
+					role: 'system',
+					content:
+						'You are a female assistant for website Learnliko. You must assist the user in using the website using given information about the website. All of your answers must be in English language. Do not answer in Thai language.'
+				},
+				{ role: 'assistant', content: 'Hi. How can I help you?' }
+			];
+		}
+	};
+	$: currentLanguage, updateCurrentLanguage();
+
 	const hide = () =>
 		showModal(ConfirmModal, {
 			title: 'Confirm',
@@ -26,11 +54,10 @@
 			onConfirm: () => ($showChatbox = false)
 		});
 
-	const submitText = async () => {
-		if (currentText.length === 0 || waitingForAIResponse) return;
+	const submitText = async (text: string) => {
+		if (text.length === 0 || waitingForAIResponse) return;
 
-		history = [...history, { role: 'user', content: currentText }];
-		currentText = '';
+		history = [...history, { role: 'user', content: text }];
 
 		waitingForAIResponse = true;
 		const assistantAnswer = await assistantChat(history);
@@ -38,18 +65,47 @@
 		waitingForAIResponse = false;
 	};
 
+	const submitCurrentText = () => {
+		if (currentText.length === 0 || waitingForAIResponse) return;
+
+		let toSubmit = currentText;
+		currentText = '';
+
+		submitText(toSubmit);
+	};
+
 	const onKeyPressed = (e: KeyboardEvent) => {
-		if (e.key === 'Enter') submitText();
+		if (e.key === 'Enter') submitCurrentText();
 	};
 
 	// assistantChat(history).then(console.log).catch(console.log);
 </script>
 
 <div
-	class="z-10 flex items-center justify-center w-full h-[48px] font-bold text-md border-b border-black/[0.15] relative"
+	class="z-10 px-4 flex items-center justify-between w-full h-[48px] font-bold text-md border-b border-black/[0.15] relative"
 >
 	<h1>Assistant</h1>
-	<div class="flex flex-row absolute right-0">
+
+	<div class="flex flex-row">
+		<div class="flex flex-row mr-2 rounded-lg overflow-hidden text-sm">
+			<button
+				on:click={() => (currentLanguage = 'TH')}
+				class={`px-2 py-1 ${
+					currentLanguage === 'TH' ? 'bg-black text-white' : 'bg-[#EFEFEF] text-black'
+				}`}
+			>
+				TH
+			</button>
+			<button
+				on:click={() => (currentLanguage = 'EN')}
+				class={`px-2 py-1 ${
+					currentLanguage === 'EN' ? 'bg-black text-white' : 'bg-[#EFEFEF] text-black'
+				}`}
+			>
+				EN
+			</button>
+		</div>
+
 		<button
 			on:click={hide}
 			class="rounded-full border border-black/[0.15] h-[28px] aspect-square text-sm"
@@ -63,7 +119,7 @@
 	{#each history as chat, index (index)}
 		{#if chat.role !== 'system'}
 			<div
-				class={`flex pt-6 flex-row items-center w-full ${
+				class={`flex flex-row mt-3 items-center w-full ${
 					chat.role === 'user' ? 'flex-row-reverse' : ''
 				}`}
 			>
@@ -81,7 +137,9 @@
 					/>
 				{/if}
 
-				<div class={`mx-3 max-w-[75%] border border-black/[0.15] py-2 px-5 rounded-xl`}>{chat.content}</div>
+				<div class={`mx-3 max-w-[75%] border border-black/[0.15] py-2 px-5 rounded-xl`}>
+					{chat.content}
+				</div>
 			</div>
 		{/if}
 	{/each}
@@ -96,21 +154,25 @@
 			<Typewriter mode="loop">...</Typewriter>
 		</div>
 	{/if}
+
+	<!-- bottom spacing -->
+	<div class="w-full h-[48px]" />
 </div>
 
-<div
-	class="absolute bottom-[86px] flex flex-row gap-6 font-line-seed w-full px-4 overflow-x-auto overflow-y-hidden"
->
-	<button class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap">
-		How to start a conversation?
-	</button>
-	<button class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap">
-		How to start a conversation?
-	</button>
-	<button class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap">
-		How to start a conversation?
-	</button>
-</div>
+{#if history.length <= 2}
+	<div
+		class="absolute bottom-[86px] flex flex-row gap-6 font-line-seed w-full px-4 overflow-x-auto overflow-y-hidden"
+	>
+		{#each hintPrompts as hint, index (index)}
+			<button
+				on:click={() => submitText(hint)}
+				class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap"
+			>
+				{hint}
+			</button>
+		{/each}
+	</div>
+{/if}
 
 <div class="w-full h-[64px] font-line-seed">
 	<div class="flex flex-row">
@@ -121,6 +183,7 @@
 			type="text"
 		/>
 
-		<button on:click={submitText} class="bg-[#9BA1FD] py-1 px-3 rounded-xl ml-3">Send</button>
+		<button on:click={submitCurrentText} class="bg-[#9BA1FD] py-1 px-3 rounded-xl ml-3">Send</button
+		>
 	</div>
 </div>

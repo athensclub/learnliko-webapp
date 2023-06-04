865d2c3b18f88402fdb95062f7f3fb58fc9b45f1
athensclub <athens_club@hotmail.com>
2023-06-05 01:47:02 +0700

    feat: chatting with assistant

--- src/lib/components/chatbox/AssistantView.svelte
+++ src/lib/components/chatbox/AssistantView.svelte
@@ -4,14 +4,20 @@
 	import ConfirmModal from '../modals/ConfirmModal.svelte';
 	import assistantProfileImage from '$lib/images/assistant.png';
 	import userProfileImage from '$lib/images/sample_kid_image.png';
+	import { assistantChat } from '$api/conversation';
+	import type { ChatMessage } from '$lib/types/requests/chatCompletion';
+	import Typewriter from 'svelte-typewriter/Typewriter.svelte';
 
-	let history: {
-		role: 'user' | 'assistant';
-		text: string;
-	}[] = [
-		{ role: 'assistant', text: 'Hello. How can I help you?' },
-		{ role: 'user', text: 'Hello. How can I help you?' }
+	let history: ChatMessage[] = [
+		{
+			role: 'system',
+			content:
+				'You are a female assistant for website Learnliko. You must assist the user in using the website using given information about the website. All of your answers must be in Thai language. Do not answer in English language.'
+		},
+		{ role: 'assistant', content: 'สวัสดี มีอะไรให้ฉันช่วยบ้าง' }
 	];
+	let currentText = '';
+	let waitingForAIResponse = false;
 
 	const hide = () =>
 		showModal(ConfirmModal, {
@@ -19,6 +25,24 @@
 			description: 'Are you sure you want to finish this conversation?',
 			onConfirm: () => ($showChatbox = false)
 		});
+
+	const submitText = async () => {
+		if (currentText.length === 0 || waitingForAIResponse) return;
+
+		history = [...history, { role: 'user', content: currentText }];
+		currentText = '';
+
+		waitingForAIResponse = true;
+		const assistantAnswer = await assistantChat(history);
+		history = [...history, { role: 'assistant', content: assistantAnswer }];
+		waitingForAIResponse = false;
+	};
+
+	const onKeyPressed = (e: KeyboardEvent) => {
+		if (e.key === 'Enter') submitText();
+	};
+
+	// assistantChat(history).then(console.log).catch(console.log);
 </script>
 
 <div
@@ -37,9 +61,9 @@
 
 <div class="w-full h-[calc(100%-48px-64px)] overflow-y-auto font-line-seed">
 	{#each history as chat, index (index)}
-		<div class={`flex flex-col px-4 w-full ${chat.role === 'user' ? 'items-end' : 'items-start'}`}>
+		{#if chat.role !== 'system'}
 			<div
-				class={`flex pt-6 flex-row items-center  w-full ${
+				class={`flex pt-6 flex-row items-center w-full ${
 					chat.role === 'user' ? 'flex-row-reverse' : ''
 				}`}
 			>
@@ -57,28 +81,46 @@
 					/>
 				{/if}
 
-				<div class={`mx-3 border border-black/[0.15] py-2 px-5 rounded-xl`}>{chat.text}</div>
+				<div class={`mx-3 max-w-[75%] border border-black/[0.15] py-2 px-5 rounded-xl`}>{chat.content}</div>
 			</div>
-		</div>
+		{/if}
 	{/each}
+
+	{#if waitingForAIResponse}
+		<div class="flex flex-row items-center">
+			<div
+				class={`w-[42px] h-[42px] bg-center bg-cover rounded-full mr-3`}
+				style="background-image: url('{assistantProfileImage}'), linear-gradient(#9BA1FD, #FFA7A7);"
+			/>
+			Thinking
+			<Typewriter mode="loop">...</Typewriter>
+		</div>
+	{/if}
 </div>
 
-<div class="absolute bottom-[86px] flex flex-row gap-6 font-line-seed w-full px-4 overflow-x-auto overflow-y-hidden">
-    <button class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap">
-        How to start a conversation?
-    </button>
-    <button class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap">
-        How to start a conversation?
-    </button>
-    <button class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap">
-        How to start a conversation?
-    </button>
+<div
+	class="absolute bottom-[86px] flex flex-row gap-6 font-line-seed w-full px-4 overflow-x-auto overflow-y-hidden"
+>
+	<button class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap">
+		How to start a conversation?
+	</button>
+	<button class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap">
+		How to start a conversation?
+	</button>
+	<button class="text-sm border border-black/[0.15] py-2 px-5 rounded-2xl whitespace-nowrap">
+		How to start a conversation?
+	</button>
 </div>
 
 <div class="w-full h-[64px] font-line-seed">
 	<div class="flex flex-row">
-		<input class="bg-[#E6E6E657] h-fit flex-1 text-lg rounded-3xl px-5 py-1" type="text" />
+		<input
+			on:keypress={onKeyPressed}
+			bind:value={currentText}
+			class="bg-[#E6E6E657] h-fit flex-1 text-lg rounded-3xl px-5 py-1"
+			type="text"
+		/>
 
-		<button class="bg-[#9BA1FD] py-1 px-3 rounded-xl ml-3">Send</button>
+		<button on:click={submitText} class="bg-[#9BA1FD] py-1 px-3 rounded-xl ml-3">Send</button>
 	</div>
 </div>

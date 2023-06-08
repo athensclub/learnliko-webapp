61c94dab8e4e0551a6dc996979b2ca7db577f777
athensclub <athens_club@hotmail.com>
2023-06-08 18:53:04 +0700

    edit: transcription endpoint using new api

--- src/routes/api/_lib/transcription.ts
+++ src/routes/api/_lib/transcription.ts
@@ -4,55 +4,10 @@
  * @returns the text transcribed from the audio.
  */
 export const transcribe = async function (audio: Blob): Promise<string> {
-	let returnedData = '',
-		transcriptId = '';
-	let attempt = 0;
-
-	while (true) {
-		try {
-			const response = await fetch('/api/v1/transcribe', {
-				method: 'POST',
-				body: audio
-			});
-
-			transcriptId = await response.text();
-			break;
-		} catch (error) {
-			// max attempt at 5
-			if (attempt++ >= 5) break;
-
-			console.error('error: uploading audio to assembly, retring...');
-		}
-	}
-
-	if (!transcriptId) throw new Error('Cannot upload audio to assembly');
-
-	attempt = 0;
-	while (true) {
-		try {
-			let response = null;
-
-			// TODO: Implement limit attempt to prevent infinit loop
-			while (true) {
-				const _res = await fetch(`/api/v1/transcribe/${transcriptId}`, {
-					method: 'GET'
-				});
-				response = await _res.text();
-
-				if (response) break;
-
-				await new Promise((resolve) => setTimeout(resolve, 3000));
-			}
-
-			returnedData = response;
-			break;
-		} catch (error) {
-			// max attempt at 5
-			if (attempt++ >= 5) break;
-
-			console.error('error: getting transcript audio, retring...');
-		}
-	}
-
-	return returnedData;
+	const response = await fetch('/api/v1/transcribe', {
+		method: 'POST',
+		body: audio
+	});
+	const { text } = await response.json();;
+	return text;
 };
--- src/routes/api/v1/transcribe/+server.ts
+++ src/routes/api/v1/transcribe/+server.ts
@@ -1,15 +1,16 @@
+import { transcribe } from '$lib/server/openai';
 import type { RequestHandler } from './$types';
-import { transcribe } from '$lib/server/assemblyai/transcription';
-import { error } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const audioBlob = await request.blob();
 	if (!audioBlob) throw error(400, `Error: No audio provided`);
 
 	try {
-		const id = await transcribe(audioBlob);
-		return new Response(id);
+		const result = await transcribe(audioBlob);
+		return json({ text: result });
 	} catch (e) {
+		console.log(e);
 		throw error(500, `Error: ${e}`);
 	}
 };

bebecaf339e62f5d0a94664f486cf38dd0b8b9d4
athensclub <athens_club@hotmail.com>
2023-06-15 09:31:16 +0700

    edit: separate "All" reading between student and professional

--- src/routes/api/_lib/reading.ts
+++ src/routes/api/_lib/reading.ts
@@ -11,13 +11,13 @@ export const getReadingTopics = async (): Promise<string[]> => {
 }
 
 export const getReadingItems = async (topic: string): Promise<ReadingItem[]> => {
-	const response = await fetch('/api/v1/reading?' + new URLSearchParams({ topic }));
+	const response = await fetch('/api/v1/reading?' + new URLSearchParams({ topic, mode: get(currentMode) }));
 	const val = await response.json();
 	return val;
 };
 
 export const getReadingItemById = async (id: string): Promise<ReadingItem> => {
-	const response = await fetch('/api/v1/reading?' + new URLSearchParams({ id }));
+	const response = await fetch('/api/v1/reading?' + new URLSearchParams({ id, mode: get(currentMode) }));
 	const val = await response.json();
 	return val;
 };

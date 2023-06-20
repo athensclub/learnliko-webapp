aba9080e14284f9f67d1f358115d75823e968f1d
athensclub <athens_club@hotmail.com>
2023-06-20 20:39:12 +0700

    add: image matching quiz view

---
+++ src/routes/pretest/ImageMatchingQuizView.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { ImageMatchingMultipleChoicesQuestion } from '$lib/types/pretest';
+
+	export let item: ImageMatchingMultipleChoicesQuestion;
+</script>
+
+<div class="flex flex-row gap-[5vw] h-[60vh] mx-auto mt-[4vh]">
+	<img class="h-full" src={item.image} alt="Quiz" />
+
+	<div class="flex flex-col h-full gap-[4vh]">
+		{#each item.choices as choice, index (index)}
+			<button
+				class={`min-w-[20vw] flex-1 rounded-full border-[0.25vw] text-[1.7vw] border-[#6C80E8] hover:bg-[#6C80E8] hover:text-white bg-white text-black`}
+			>
+				{choice}
+			</button>
+		{/each}
+	</div>
+</div>

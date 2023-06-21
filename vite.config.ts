3ca60d1a2bd9bce9c6c2345c0717bf190fbc1242
athensclub <athens_club@hotmail.com>
2023-06-21 23:29:39 +0700

    build: setup scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit(), nodePolyfills({ protocolImports: true, }),],
+	plugins: [sveltekit(), nodePolyfills({ protocolImports: true })],
+
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@use "src/variables.scss" as *;'
+			}
+		}
 	}
 });

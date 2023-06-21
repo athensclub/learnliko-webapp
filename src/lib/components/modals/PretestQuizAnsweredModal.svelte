77e507fe384eb96152a4cc1ed14339b37dfbcde4
athensclub <athens_club@hotmail.com>
2023-06-21 23:12:18 +0700

    add: Pretest quiz answered modal

---
+++ src/lib/components/modals/PretestQuizAnsweredModal.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type {
+		FillInTheBlankMultipleChoicesQuestion,
+		ImageMatchingMultipleChoicesQuestion
+	} from '$lib/types/pretest';
+
+	export let quiz: FillInTheBlankMultipleChoicesQuestion | ImageMatchingMultipleChoicesQuestion;
+	export let userAnswer: number;
+	export let nextClicked: () => void;
+</script>
+
+<div class="w-full h-full bg-white font-line-seed flex flex-col" >
+    
+</div>

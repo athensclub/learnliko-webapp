dde5ac4076c95049ece1ae93594ba491b7eb5f5d
athensclub <athens_club@hotmail.com>
2023-06-23 02:57:11 +0700

    add: cefr pretest level utility functions

--- src/lib/utils/cefr.ts
+++ src/lib/utils/cefr.ts
@@ -1,6 +1,9 @@
 import type { CEFRLevel } from "$lib/types/CEFRLevel";
+import type { PretestCEFRLevel } from "$lib/types/pretest";
 
 const ordered: CEFRLevel[] = ['pre-A1', 'A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
+
+const orderedPretest: PretestCEFRLevel[] = ['pre-A1', 'A1', 'A2', 'B1', 'B2']
 
 /**
  * 
@@ -19,5 +22,25 @@ export const nextLevel = (level: CEFRLevel) => {
  */
 export const previousLevel = (level: CEFRLevel) => {
     const idx = ordered.indexOf(level);
-    return idx - 1 >= 0 ? ordered[i - 1] : level;
+    return idx - 1 >= 0 ? ordered[idx - 1] : level;
+}
+
+/**
+ * 
+ * @param level 
+ * @returns the level that is a level above the given level. this function will return the same level if there's no higher level than the given level.
+ */
+export const nextPretestLevel = (level: PretestCEFRLevel) => {
+    const idx = orderedPretest.indexOf(level);
+    return idx + 1 < orderedPretest.length ? orderedPretest[idx + 1] : level;
+}
+
+/**
+ * 
+ * @param level 
+ * @returns the level that is a level below the given level. this function will return the same level if there's no lower level than the given level.
+ */
+export const previousPretestLevel = (level: PretestCEFRLevel) => {
+    const idx = orderedPretest.indexOf(level);
+    return idx - 1 >= 0 ? orderedPretest[idx - 1] : level;
 }

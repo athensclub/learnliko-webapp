e01dc2782cd223a74758c2118eb9db0b1734eed2
kithe-raker <kithe.raker@gmail.com>
2023-06-22 20:56:54 +0700

    edit: include `UserProfile` prop in `userSession` store

--- src/lib/stores/userSession.ts
+++ src/lib/stores/userSession.ts
@@ -1,17 +1,24 @@
 import { writable, get } from 'svelte/store';
 import type firebase from 'firebase/auth';
+import type { UserProfile } from '$lib/types/userProfile';
 
-const userSession = writable<{
+interface UserSessionData {
 	initialized?: boolean;
 	isLoggedIn?: boolean;
 	authUser?: firebase.User | null;
-}>({
+	profile?: UserProfile | null;
+}
+
+const userSession = writable<UserSessionData>({
 	initialized: false,
-	isLoggedIn: false,
+	isLoggedIn: false
 });
 
 export default {
 	subscribe: userSession.subscribe,
 	set: userSession.set,
+	update: (data: UserSessionData) => {
+		userSession.set({ ...get(userSession), ...data });
+	},
 	value: () => get(userSession)
 };

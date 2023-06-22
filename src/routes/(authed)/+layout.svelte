dc9a5a42ab22050e033a69b22066904f3c04a21b
kithe-raker <kithe.raker@gmail.com>
2023-06-22 21:57:46 +0700

    edit: grouping register route

---
+++ src/routes/(authed)/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import userSession from '$lib/stores/userSession';
+	import { onMount } from 'svelte';
+
+	let loading = true;
+
+	onMount(() => {
+		// Subscribe on user's session
+		userSession.subscribe((session) => {
+			// ensure the initialization is completed
+			if (!session.initialized) {
+				loading = true;
+				return;
+			}
+
+			// navigate to homepage if user is not logged in
+			if (!session.isLoggedIn) goto(`/`);
+
+			loading = false;
+		});
+	});
+</script>
+
+{#if !loading}
+	<slot />
+{/if}

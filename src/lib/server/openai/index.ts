019842304de3260364dd71ce2b5d524a8f737701
kithe-raker <kithe.raker@gmail.com>
2023-06-23 12:40:42 +0700

    add: create `gptFunctionCalling` function #13

--- src/lib/server/openai/index.ts
+++ src/lib/server/openai/index.ts
@@ -1,6 +1,11 @@
 import { SECRET_OPENAI_API_KEY } from '$env/static/private';
 import type { ChatMessage } from '$lib/types/requests/chatCompletion';
-import { Configuration, OpenAIApi } from 'openai';
+import {
+	Configuration,
+	OpenAIApi,
+	type ChatCompletionFunctions,
+	type CreateChatCompletionRequestFunctionCall
+} from 'openai';
 import { Readable } from 'stream';
 
 const _configuration = new Configuration({
@@ -18,6 +23,21 @@ export const chatCompletion = async function (messages: ChatMessage[]) {
 	return chatGPT.data.choices[0].message?.content;
 };
 
+export const gptFunctionCalling = async function (
+	messages: ChatMessage[],
+	functions: ChatCompletionFunctions[],
+	function_call: CreateChatCompletionRequestFunctionCall
+) {
+	const chatGPT = await _openai.createChatCompletion({
+		model: 'gpt-3.5-turbo-0613',
+		function_call,
+		functions,
+		messages
+	});
+
+	return chatGPT.data.choices[0].message?.content;
+};
+
 export const transcribe = async function (audio: Blob) {
 	// modified from https://github.com/openai/openai-node/issues/77#issuecomment-1500899486
 	const buffer = await audio.arrayBuffer();
@@ -25,9 +45,16 @@ export const transcribe = async function (audio: Blob) {
 		read() {
 			this.push(Buffer.from(buffer));
 			this.push(null);
-		},
+		}
 	});
 	readable.path = 'audio.webm';
-	const response = await _openai.createTranscription(readable, 'whisper-1', undefined, undefined, undefined, 'en');
+	const response = await _openai.createTranscription(
+		readable,
+		'whisper-1',
+		undefined,
+		undefined,
+		undefined,
+		'en'
+	);
 	return response.data.text;
-}
+};

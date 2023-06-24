5eea0b7797f496e2964241e49d37ed6b01aa9cc7
athensclub <athens_club@hotmail.com>
2023-06-24 15:48:35 +0700

    fix: cant play conversation in pretest

--- src/lib/global/conversation.ts
+++ src/lib/global/conversation.ts
@@ -148,7 +148,7 @@ const botReply = async function (message?: string) {
 	}
 
 	// behavior regarding bot's message status
-	if ((ct && get(currentGoal) >= ct.conversation.details.learner.goal.length) || get(history).length >= 2 * get(maxDialogueCount)) {
+	if ((get(isCheckConversationGoal) && get(currentGoal) >= ct.conversation.details.learner.goal.length) || get(history).length >= 2 * get(maxDialogueCount)) {
 		conversationFinished.set(true);
 		if (get(saveCurrentConversation)) {
 			finishedTime = new Date();

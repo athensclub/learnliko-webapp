ec10e22bbec7c5c4179a9c6b87d5e54a8825d58e
athensclub <athens_club@hotmail.com>
2023-06-29 02:08:22 +0700

    edit: entering lesson transition loading view

--- src/lib/components/loading/EnteringLesson.svelte
+++ src/lib/components/loading/EnteringLesson.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { chatContext } from '$lib/global/chatbox';
 	import { fade } from 'svelte/transition';
+	import background from '$lib/images/traveling_background.mp4';
 
 	export let onFinish: () => void;
 
@@ -16,26 +17,29 @@
 
 <div
 	transition:fade
-	class="w-[100vw] h-[100vh] px-[10vw] bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] flex flex-row justify-between items-center font-line-seed"
+	class="w-[100vw] h-[100vh] px-[10vw] bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] relative flex flex-row justify-between items-center font-line-seed overflow-hidden"
 >
-	<div
+	<!-- <div
 		style="background-color: #FFD281; background-image: url('{$chatContext?.conversation.avatar
 			.models.neutral}')"
 		class="w-[10vw] h-[10vw] bg-top bg-cover rounded-full border border-white"
-	/>
+	/> -->
+	<video autoplay muted loop class="absolute left-0 top-0 w-full h-full object-cover">
+		<source src={background} type="video/mp4" />
+	</video>
 
-	<div class="flex flex-col items-center text-white text-[1.5vw] font-bold">
+	<div class="absolute left-[50%] translate-x-[-50%] flex flex-col items-center text-white text-[1.5vw] font-bold">
 		จะถึงภายใน {countdown} วินาที
 		<iframe
 			title="Moving Car"
-			class="w-[35vw] h-[35vw] my-[2vh] rounded-[4vw]"
+			class="w-[35vw] h-[35vw] my-[2vh] rounded-full"
 			src="https://rive.app/community/110-170-flying-car/embed"
 		/>
-		กำลังเดินทางไปยังเพื่อนของคุณ เตรียมตัวให้พร้อม
+		กำลังเดินทาง เตรียมตัวให้พร้อม
 	</div>
 
-	<div
+	<!-- <div
 		style="background-image: url('{$chatContext?.conversation.background}')"
 		class="w-[10vw] h-[10vw] bg-center bg-cover rounded-full border border-white"
-	/>
+	/> -->
 </div>

864ac2d784a5c1ee9c4c5fc381b2d68937eed08d
kithe-raker <kithe.raker@gmail.com>
2023-07-04 20:29:19 +0700

    add: create `signInWithUsername` function

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,10 +1,19 @@
 import { auth } from '$lib/configs/firebase.config';
 import { authErrorCodeMapping } from '$lib/utils/authError';
-import { signInAnonymously, signOut } from 'firebase/auth';
+import { signInAnonymously, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 export const signInAnonymous = async function () {
 	try {
 		const result = await signInAnonymously(auth);
+		return result;
+	} catch (error) {
+		throw new Error(authErrorCodeMapping((error as Error).message));
+	}
+};
+
+export const signInWithUsername = async function (username: string, password: string) {
+	try {
+		const result = await signInWithEmailAndPassword(auth, `${username}@learnliko.demo`, password);
 		return result;
 	} catch (error) {
 		throw new Error(authErrorCodeMapping((error as Error).message));

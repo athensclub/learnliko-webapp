e622b068a4f938aa2332be2a2171b776b2c61ba4
kithe-raker <kithe.raker@gmail.com>
2023-07-07 00:33:21 +0700

    add: add `create quiz recap` schema

--- src/gql/schema/mutations/index.ts
+++ src/gql/schema/mutations/index.ts
@@ -1,7 +1,7 @@
 import { graphql } from '$gql/generated';
 
 export const CREATE_USER_ACCOUNT = graphql(`
-	mutation Mutation($data: UserCreateDataInput!) {
+	mutation CreateUserAccount($data: UserCreateDataInput!) {
 		userCreate(data: $data) {
 			uid
 			username
@@ -36,6 +36,43 @@ export const CREATE_USER_ACCOUNT = graphql(`
 				id
 				name
 			}
+		}
+	}
+`);
+
+export const RECAP_VOCAB_QUIZ = graphql(`
+	mutation RecapVocabQuiz($data: VocabularyRecapCreateDataInput!, $uid: ID!) {
+		vocabularyRecapCreate(data: $data, uid: $uid) {
+			correct
+		}
+	}
+`);
+
+export const RECAP_SENTENCE_QUIZ = graphql(`
+	mutation RecapSentenceQuiz($uid: ID!, $data: SentenceRecapCreateDataInput!) {
+		sentenceRecapCreate(uid: $uid, data: $data) {
+			correct
+		}
+	}
+`);
+
+export const RECAP_READING_QUIZ = graphql(`
+	mutation RecapReadingQuiz($data: ReadingRecapCreateDataInput!, $uid: ID!) {
+		readingRecapCreate(data: $data, uid: $uid) {
+			totalCorrect
+			answer {
+				userAnswer
+				answerIndex
+				correct
+			}
+		}
+	}
+`);
+
+export const RECAP_CONVERSATION_QUIZ = graphql(`
+	mutation RecapConversationQuiz($data: ConversationRecapCreateDataInput!, $uid: ID!) {
+		conversationRecapCreate(data: $data, uid: $uid) {
+			correctPercentage
 		}
 	}
 `);

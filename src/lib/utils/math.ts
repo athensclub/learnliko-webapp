5337810c9cbff81e756d8fe335860b8faf9ebc8d
athensclub <athens_club@hotmail.com>
2023-07-06 19:50:57 +0700

    add: random int range function

--- src/lib/utils/math.ts
+++ src/lib/utils/math.ts
@@ -8,3 +8,7 @@ export const round = (x: number, digits: number) => {
     const tenPow = Math.pow(10, digits);
     return Math.round((x + Number.EPSILON) * tenPow) / tenPow;
 }
+
+export const randomIntInclusive = (min: number, max: number) => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}

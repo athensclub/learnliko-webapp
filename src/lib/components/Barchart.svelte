e2fb0f9dfaca492dc0d5bdbe60a96d6796860817
kithe-raker <kithe.raker@gmail.com>
2023-07-06 20:12:21 +0700

    edit: implement overall data getter #19

--- src/lib/components/Barchart.svelte
+++ src/lib/components/Barchart.svelte
@@ -2,33 +2,17 @@
 	import * as d3 from 'd3';
 	import { fly } from 'svelte/transition';
 
+	export let activeUseToday = 0;
+
 	const alphabet = [
-		{ letter: 'A', frequency: 0.08167 },
-		{ letter: 'B', frequency: 0.01492 },
-		{ letter: 'C', frequency: 0.02782 },
-		{ letter: 'D', frequency: 0.04253 },
-		{ letter: 'E', frequency: 0.12702 },
-		{ letter: 'F', frequency: 0.02288 },
-		{ letter: 'G', frequency: 0.02015 },
-		{ letter: 'H', frequency: 0.06094 },
-		{ letter: 'I', frequency: 0.06966 },
-		{ letter: 'J', frequency: 0.00153 },
-		{ letter: 'K', frequency: 0.00772 },
-		{ letter: 'L', frequency: 0.04025 },
-		{ letter: 'M', frequency: 0.02406 },
-		{ letter: 'N', frequency: 0.06749 },
-		{ letter: 'O', frequency: 0.07507 },
-		{ letter: 'P', frequency: 0.01929 },
-		{ letter: 'Q', frequency: 0.00095 },
-		{ letter: 'R', frequency: 0.05987 },
-		{ letter: 'S', frequency: 0.06327 },
-		{ letter: 'T', frequency: 0.09056 },
-		{ letter: 'U', frequency: 0.02758 },
-		{ letter: 'V', frequency: 0.00978 },
-		{ letter: 'W', frequency: 0.0236 },
-		{ letter: 'X', frequency: 0.0015 },
-		{ letter: 'Y', frequency: 0.01974 },
-		{ letter: 'Z', frequency: 0.00074 }
+		{ letter: '30/06', frequency: 0 },
+		{ letter: '1/07', frequency: 0 },
+		{ letter: '2/07', frequency: 0 },
+		{ letter: '3/07', frequency: 0 },
+		{ letter: '4/07', frequency: 0 },
+		{ letter: '5/07', frequency: 0 },
+		{ letter: '6/07', frequency: 0 },
+		{ letter: 'วันนี้', frequency: activeUseToday }
 	];
 
 	let data = alphabet;
--- src/routes/dashboard/overall/+page.svelte
+++ src/routes/dashboard/overall/+page.svelte
@@ -1,144 +1,145 @@
 <script lang="ts">
 	import NavBar from '$lib/components/navbar/NavBar.svelte';
 	import Barchart from '$lib/components/Barchart.svelte';
+	import { getActiveUsers, getTotalLearner, queryLearnersByExp } from '$lib/temp/analytic';
+	import { onMount } from 'svelte';
+	import type { User } from '$gql/graphql';
 
 	let activeUse = 64;
 	let averageCEFR = 'A1';
 	let totalStudents = 150;
 
-	let leaderboard = [
-		{
-			username: 'Natsataporn M.',
-			exp: 30,
-			profileImage:
-				'https://cdn.discordapp.com/attachments/842737146321174558/1124349468247068764/image.png',
-			background:
-			'https://media.discordapp.net/attachments/1120787872379842711/1126187400943644712/image.png'
-		},
-		{
-			username: 'Natsataporn M.',
-			exp: 25,
-			profileImage:
-				'https://cdn.discordapp.com/attachments/842737146321174558/1124349032685379605/image.png',
-			background:
-			'https://media.discordapp.net/attachments/1120787872379842711/1126187400943644712/image.png'
-		},
-		{
-			username: 'Natsataporn M.',
-			exp: 25,
-			profileImage:
-				'https://cdn.discordapp.com/attachments/842737146321174558/1124349032685379605/image.png',
-			background:
-			'https://media.discordapp.net/attachments/1120787872379842711/1126187400943644712/image.png'
-		},
-		{
-			username: 'Natsataporn M.',
-			exp: 25,
-			profileImage:
-				'https://cdn.discordapp.com/attachments/842737146321174558/1124349032685379605/image.png',
-			background:
-			'https://media.discordapp.net/attachments/1120787872379842711/1126187400943644712/image.png'
-		},
-		{
-			username: 'Natsataporn M.',
-			exp: 25,
-			profileImage:
-				'https://cdn.discordapp.com/attachments/842737146321174558/1124349032685379605/image.png',
-			background:
-			'https://media.discordapp.net/attachments/1120787872379842711/1126187400943644712/image.png'
-		},
-	];
+	let leaderboard: User[] = [];
+	let loading = true;
+
+	onMount(() => {
+		initializeData();
+	});
+
+	const initializeData = async function () {
+		await Promise.all([
+			getTotalLearner().then((learners) => {
+				totalStudents = learners.totalLearner;
+				averageCEFR = learners.averageLevel;
+			}),
+
+			getActiveUsers().then((active) => {
+				activeUse = active;
+			}),
+
+			queryLearnersByExp().then((learners) => {
+				learners.forEach((data) => {
+					if (data.profile) leaderboard.push(data);
+				});
+			})
+		]);
+		loading = false;
+	};
 </script>
 
-
-<div class="flex w-full flex-row bg-[#F4F4F4] font-line-seed">
-	<NavBar spaced teacher />
-	<div class=" container w-[70vw] mx-auto">
-		<div class="flex flex-1 py-[2vw] text-white">
-			<div class=" mr-[2vw] h-[10vw] w-1/3 rounded-[1.25vw] shadow-lg bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] p-[1.5vw]">
-				<h1 class=" text-[1.25vw]">ผู้เรียนที่เข้าใช้งานวันนี้</h1>
-				<h1 class=" text-[3.25vw] font-extrabold">{activeUse}</h1>
+{#if loading}
+	Loading...
+{:else}
+	<div class="flex w-full flex-row bg-[#F4F4F4] font-line-seed">
+		<NavBar spaced teacher />
+		<div class=" container mx-auto w-[70vw]">
+			<div class="flex flex-1 py-[2vw] text-white">
+				<div
+					class=" mr-[2vw] h-[10vw] w-1/3 rounded-[1.25vw] bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] p-[1.5vw] shadow-lg"
+				>
+					<h1 class=" text-[1.25vw]">ผู้เรียนที่เข้าใช้งานวันนี้</h1>
+					<h1 class=" text-[3.25vw] font-extrabold">{activeUse}</h1>
+				</div>
+				<div
+					class=" mr-[2vw] h-[10vw] w-1/3 rounded-[1.25vw] bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] p-[1.5vw] shadow-lg"
+				>
+					<h1 class=" text-[1.25vw]">ค่าเฉลี่ย CEFR ของนักเรียน</h1>
+					<h1 class=" text-[3.25vw] font-extrabold">{averageCEFR}</h1>
+				</div>
+				<div
+					class=" mr-[2vw] h-[10vw] w-1/3 rounded-[1.25vw] bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] p-[1.5vw] shadow-lg"
+				>
+					<h1 class=" text-[1.25vw]">นักเรียนทั้งหมด</h1>
+					<h1 class=" text-[3.25vw] font-extrabold">{totalStudents}</h1>
+				</div>
 			</div>
-			<div class=" mr-[2vw] h-[10vw] w-1/3 rounded-[1.25vw] shadow-lg bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] p-[1.5vw]">
-				<h1 class=" text-[1.25vw]">ค่าเฉลี่ย CEFR ของนักเรียน</h1>
-				<h1 class=" text-[3.25vw] font-extrabold">{averageCEFR}</h1>
-			</div>
-			<div class=" mr-[2vw] h-[10vw] w-1/3 rounded-[1.25vw] shadow-lg bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] p-[1.5vw]">
-				<h1 class=" text-[1.25vw]">นักเรียนทั้งหมด</h1>
-				<h1 class=" text-[3.25vw] font-extrabold">{totalStudents}</h1>
-			</div>
-		</div>
 
-		<div class="flex flex-1 ">
-			<div class=" mr-[2vw] h-[28vw] pb-[3vw] w-1/3 rounded-[1.25vw] shadow-lg bg-white overflow-scroll overflow-x-hidden">
-				<h1 class=" text-[1.5vw] px-[1vw] py-[1vw] font-bold">Leader board</h1>
+			<div class="flex flex-1">
+				<div
+					class=" mr-[2vw] h-[28vw] w-1/3 overflow-scroll overflow-x-hidden rounded-[1.25vw] bg-white pb-[3vw] shadow-lg"
+				>
+					<h1 class=" px-[1vw] py-[1vw] text-[1.5vw] font-bold">Leader board</h1>
 
-				<div class="w-[100%] h-full bg-white rounded-[2vw] flex flex-col font-bold">
-				
-					<div class="flex flex-col px-[0.8vw] gap-[1vw] overflow-y-auto">
-						{#each leaderboard as user, index (index)}
-							<!-- Somehow I have to specify the min height to fix the height -->
-							<div
-								style="background-image: url('{user.background}');"
-								class="w-full h-[5.5vw]  px-[1vw] bg-cover bg-center rounded-[1vw] flex flex-row items-center justify-between"
-							>
-								<div class="flex flex-row items-center gap-[0.5vw] h-full">
+					<div class="flex h-full w-[100%] flex-col rounded-[2vw] bg-white font-bold">
+						<div class="flex flex-col gap-[1vw] overflow-y-auto px-[0.8vw]">
+							{#each leaderboard as user, index (index)}
+								<!-- Somehow I have to specify the min height to fix the height -->
+								<div
+									style="background-image: url('{user.profile?.coverUrl}');"
+									class="flex h-[5.5vw] w-full flex-row items-center justify-between rounded-[1vw] bg-cover bg-center px-[1vw]"
+								>
+									<div class="flex h-full flex-row items-center gap-[0.5vw]">
+										<div
+											style="background-image: url('{user.profile?.imageUrl}');"
+											class="aspect-square h-[50%] rounded-full border-[0.05vw] border-white bg-cover bg-center"
+										/>
+										<div
+											class="flex h-fit max-w-[10vw] items-center rounded-full bg-white px-[1vw] py-[0.3vw]"
+										>
+											<!-- Somehow putting text inside another layer of div make ellipsis works -->
+											<div class="w-fit truncate whitespace-nowrap text-[0.7vw]">
+												{user.profile?.fullname}
+											</div>
+										</div>
+									</div>
+
 									<div
-										style="background-image: url('{user.profileImage}');"
-										class="h-[50%] aspect-square bg-cover bg-center rounded-full border-[0.05vw] border-white"
-									/>
-									<div
-										class="h-fit max-w-[10vw] px-[1vw] py-[0.3vw] bg-white flex items-center rounded-full"
+										class="flex h-fit flex-row rounded-full bg-white px-[0.5vw] py-[0.3vw] text-[0.5vw]"
 									>
-										<!-- Somehow putting text inside another layer of div make ellipsis works -->
-										<div class="w-fit truncate text-[0.7vw] whitespace-nowrap">
-											{user.username}
+										<svg
+											class="w-[1vw]"
+											viewBox="0 0 17 17"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												d="M8.5 0L0 8.5L8.5 17L17 8.5L8.5 0ZM4.25 8.5L8.5 4.25L12.75 8.5L8.5 12.75L4.25 8.5Z"
+												fill="url(#paint0_linear_1194_8403)"
+											/>
+											<defs>
+												<linearGradient
+													id="paint0_linear_1194_8403"
+													x1="-0.159014"
+													y1="0.160377"
+													x2="11.0702"
+													y2="20.3054"
+													gradientUnits="userSpaceOnUse"
+												>
+													<stop stop-color="#C698FF" />
+													<stop offset="1" stop-color="#6C80E8" />
+												</linearGradient>
+											</defs>
+										</svg>
+										<div
+											class="ml-[0.3vw] bg-gradient-to-r from-[#C698FF] to-[#6C80E8] bg-clip-text text-[0.7vw] text-transparent"
+										>
+											{user.exp}
 										</div>
 									</div>
 								</div>
-		
-								<div class="flex flex-row h-fit bg-white rounded-full text-[0.5vw] px-[0.5vw] py-[0.3vw]">
-									<svg
-										class="w-[1vw]"
-										viewBox="0 0 17 17"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M8.5 0L0 8.5L8.5 17L17 8.5L8.5 0ZM4.25 8.5L8.5 4.25L12.75 8.5L8.5 12.75L4.25 8.5Z"
-											fill="url(#paint0_linear_1194_8403)"
-										/>
-										<defs>
-											<linearGradient
-												id="paint0_linear_1194_8403"
-												x1="-0.159014"
-												y1="0.160377"
-												x2="11.0702"
-												y2="20.3054"
-												gradientUnits="userSpaceOnUse"
-											>
-												<stop stop-color="#C698FF" />
-												<stop offset="1" stop-color="#6C80E8" />
-											</linearGradient>
-										</defs>
-									</svg>
-									<div
-										class="ml-[0.3vw] text-[0.7vw] bg-clip-text text-transparent bg-gradient-to-r from-[#C698FF] to-[#6C80E8]"
-									>
-										{user.exp}
-									</div>
-								</div>
-							</div>
-						{/each}
+							{/each}
+						</div>
+					</div>
+				</div>
+				<div
+					class=" mr-[2vw] h-[28vw] w-[44vw] rounded-[1.25vw] bg-white px-[1.5vw] pt-[1vw] shadow-lg"
+				>
+					<h1 class="text-[1.5vw]">Daily Learning Stats</h1>
+					<div class="mt-[5vh] w-full">
+						<Barchart activeUseToday={20} />
 					</div>
 				</div>
 			</div>
-			<div class=" mr-[2vw] h-[28vw] w-[44vw] rounded-[1.25vw] shadow-lg bg-white px-[1.5vw]  pt-[1vw] ">
-				<h1 class="text-[1.5vw]">Daily Learning Stats</h1>
-				<div class="w-full mt-[5vh]">
-				<Barchart></Barchart> </div>
-			</div>
 		</div>
-		
 	</div>
-</div>
+{/if}

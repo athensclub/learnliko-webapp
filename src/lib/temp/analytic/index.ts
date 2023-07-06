a06fd4d0851991f4a397ed542ee6d2890d3cb43f
kithe-raker <kithe.raker@gmail.com>
2023-07-06 19:28:31 +0700

    add: `queryLearnersByExp` function #22

--- src/lib/temp/analytic/index.ts
+++ src/lib/temp/analytic/index.ts
@@ -1,11 +1,36 @@
+import type { User } from '$gql/generated/graphql';
 import { firestore } from '$lib/configs/firebase.config';
 import type { PretestCEFRLevel } from '$lib/types/pretest';
-import { doc, getDoc, increment, setDoc } from 'firebase/firestore/lite';
+import {
+	QueryConstraint,
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	increment,
+	limit,
+	orderBy,
+	query,
+	setDoc,
+	where
+} from 'firebase/firestore/lite';
 
 const _analyticCollection = 'Analytics';
 const _level = ['PRE_A1', 'A1', 'A2', 'B1', 'B2'];
+const _classroom = [{ id: 'classroom1', name: 'Classroom 1' }];
 
 // Data Query
+export const queryLearnersByExp = async function (classroomScope?: string) {
+	const usersRef = collection(firestore, `Users`);
+	const queryConst: QueryConstraint[] = [orderBy('exp', 'desc'), limit(30)];
+	if (classroomScope) {
+		queryConst.push(where('classRoom.id', '==', classroomScope));
+	}
+	const snapshot = await getDocs(query(usersRef, ...queryConst));
+
+	return snapshot.docs.map((doc) => doc.data() as User);
+};
+
 export const getActiveUsers = async function () {
 	const docRef = doc(firestore, `${_analyticCollection}/dailyActiveUser`);
 	const result = await getDoc(docRef);

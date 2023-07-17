61a713b557746b1661bbb68d1909d0fe09de4d77
athensclub <athens_club@hotmail.com>
2023-07-17 21:59:17 +0700

    edit: new mobile design for entering lesson loading view

--- src/lib/components/loading/EnteringLesson.svelte
+++ src/lib/components/loading/EnteringLesson.svelte
@@ -4,6 +4,7 @@
 	import background from '$lib/images/traveling_background.mp4';
 	import audio from '$lib/audios/entering_lesson_transition_sound.wav';
 	import { tweened } from 'svelte/motion';
+	import { isMobile } from '$lib/global/breakpoints';
 
 	export let onFinish: () => void;
 
@@ -30,26 +31,28 @@
 
 <div
 	transition:fade
-	class="fixed z-[1000] top-0 left-0 w-[100vw] h-[100vh] px-[10vw] bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] flex flex-row justify-between items-center font-line-seed overflow-hidden"
+	class="fixed left-0 top-0 z-[1000] flex h-[100vh] w-[100vw] flex-row items-center justify-between overflow-hidden bg-gradient-to-r from-[#6C80E8] to-[#9BA1FD] px-[10vw] font-line-seed"
 >
 	<!-- <div
 		style="background-color: #FFD281; background-image: url('{$chatContext?.conversation.avatar
 			.models.neutral}')"
 		class="w-[10vw] h-[10vw] bg-top bg-cover rounded-full border border-white"
 	/> -->
-	<video autoplay muted loop class="absolute left-0 top-0 w-full h-full object-cover">
+	<video autoplay muted loop class="absolute left-0 top-0 h-full w-full object-cover">
 		<source src={background} type="video/mp4" />
 	</video>
 
 	<audio bind:this={audioInstance} autoplay src={audio} />
 
 	<div
-		class="absolute left-[50%] translate-x-[-50%] flex flex-col items-center text-white text-[1.5vw] font-bold"
+		class="absolute left-[50%] flex translate-x-[-50%] flex-col items-center font-bold text-white {$isMobile
+			? 'text-[5vw]'
+			: 'text-[1.5vw]'}"
 	>
 		จะถึงภายใน {countdown} วินาที
 		<iframe
 			title="Moving Car"
-			class="w-[35vw] h-[35vw] my-[2vh] rounded-full"
+			class="my-[2vh] rounded-full {$isMobile ? 'h-[80vw] w-[80vw]' : 'h-[35vw] w-[35vw]'}"
 			src="https://rive.app/community/110-170-flying-car/embed"
 		/>
 		กำลังเดินทาง เตรียมตัวให้พร้อม

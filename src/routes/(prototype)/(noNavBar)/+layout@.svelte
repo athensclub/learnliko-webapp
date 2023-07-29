2da4a0e4033229dff9e64480fda1124109ccf1cb
kithe-raker <kithe.raker@gmail.com>
2023-07-29 12:42:01 +0700

    feat: `play` route for enter lesson

---
+++ src/routes/(prototype)/(noNavBar)/+layout@.svelte
@@ -0,0 +1,7 @@
+<!-- 
+    Every page in this (noNavBar) group route will exclude navigation bar
+    which is indicate by this file name (+layout@.svelte) `@` indicate to put the component in the root
+    for more see https://kit.svelte.dev/docs/advanced-routing#advanced-layouts-layout
+-->
+
+<slot />

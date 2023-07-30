f7c8d6ea5f964c95fde0720be5154eaefbe1e6f2
kithe-raker <kithe.raker@gmail.com>
2023-07-30 14:05:38 +0700

    edit: choice UI section of cloze card

--- src/routes/(prototype)/(noNavBar)/play/[id]/LessonClozeActivityView.svelte
+++ src/routes/(prototype)/(noNavBar)/play/[id]/LessonClozeActivityView.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { Activity, ClozeCard } from '$gql/generated/graphql';
+	import { fade } from 'svelte/transition';
 
 	export let data: Activity;
 	export let onFinish: () => void;
@@ -15,19 +16,22 @@
 	$: textLines = currentCard.text.split('\n').map((line) => line.split('_'));
 </script>
 
-<div class="rounded-[8.21vw] bg-white">
+<div transition:fade class="rounded-[8.21vw] bg-white">
 	<!-- title -->
 	<div
 		class="flex min-h-[13vh] flex-col justify-center border-b border-[#EAEAEA] text-center text-[4.1vw] font-normal"
 	>
-		{data.title}
+		{currentCard.title}
 	</div>
 
-	<div class=" h-[28.79vh] w-full px-[8.21vw] py-[1.78vh]">
+	<!-- text renderer -->
+	<div
+		class=" h-[28.79vh] w-full overflow-x-hidden overflow-y-scroll border-b border-[#EAEAEA] px-[8.21vw] py-[1.78vh]"
+	>
 		<div class="flex h-fit w-full flex-wrap justify-start text-[4.1vw] font-bold">
 			{#each textLines as line}
 				<div class="mb-[1.3vh]">
-					<div class=" flex items-center">
+					<div class="flex items-center">
 						{#each line as part, index}
 							{part}
 
@@ -46,4 +50,23 @@
 			{/each}
 		</div>
 	</div>
+
+	<!-- choice -->
+	<div
+		class="flex h-[14.81vh] w-full flex-wrap overflow-x-hidden overflow-y-scroll border-b border-[#EAEAEA] px-[5.38vw] py-[1.9vh]"
+	>
+		{#each currentCard.choices as choice}
+			<div
+				class="mb-[1.3vh] mr-[4.1vw] flex h-[4.15vh] items-center rounded-[2.56vw] border border-[#D9D9D9] px-[5.9vw] py-[1.28vh] text-[4.1vw] font-bold"
+			>
+				{choice}
+			</div>
+		{/each}
+	</div>
+
+	<button
+		on:click={() => {
+			currentIndex = 1;
+		}}>Click Me</button
+	>
 </div>

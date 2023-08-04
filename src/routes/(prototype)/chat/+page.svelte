5e54908b5403b591f3335768c70e971097a4b7e4
athensclub <athens_club@hotmail.com>
2023-08-04 23:22:46 +0700

    add: ai friend home view

--- src/routes/(prototype)/chat/+page.svelte
+++ src/routes/(prototype)/chat/+page.svelte
@@ -1,8 +1,46 @@
-<script lang="ts">
+<script lang="ts" context="module">
+	import { writable } from 'svelte/store';
+	import type { AiFriend } from '$gql/generated/graphql';
+
+	export const aiFriendCurrentView = writable<'Home'>('Home');
+
+	export const aiFriends = writable<AiFriend[]>([]);
+
+	export type Inbox = {
+		friend: AiFriend;
+		message: string;
+	};
+	export const inboxes = writable<Inbox[]>([]);
 </script>
 
+<script lang="ts">
+	import AiFriendHomeView from './AIFriendHomeView.svelte';
+	import { onMount } from 'svelte';
 
-<div>
-	<h3>Hello</h3>
-	<p>This is Chat</p>
+	onMount(() => {
+		$aiFriendCurrentView = 'Home';
+
+		const kiko: AiFriend = {
+			name: 'KiKo',
+			role: 'Assistant',
+			subject: 'Assistant',
+			interest: 'Helping learners archive their goals with Learnliko',
+			ability: ['App Instruction'],
+			prompt:
+				'You are a helpful assistant that will guide the user in using Learnliko web application.'
+		};
+		$aiFriends = [kiko];
+		$inboxes = [
+			{
+				friend: kiko,
+				message: 'สวัสดี ยินดีต้อรับสู่ Learnliko นี่คือคำแนะนำเบื้องต้น...'
+			}
+		];
+	});
+</script>
+
+<div class="w-full flex-1 font-line-seed">
+	{#if $aiFriendCurrentView === 'Home'}
+		<AiFriendHomeView />
+	{/if}
 </div>
